eed39ea49da11844586686d10760de7540c098ef
GwangYoung <gospotb1607@gmail.com>
2022-08-24 21:36:53 +0900

    view_url &  command
    view_url &  command

--- DJANGO_LECTURES/Portfolio/my_app/urls.py
+++ DJANGO_LECTURES/Portfolio/my_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# 기본적으로 path 속 ''에는 my_app/ 이 포함된 상황임.
 urlpatterns = [
     path('',views.index,name='index')
 ]
--- DJANGO_LECTURES/Portfolio/my_app/views.py
+++ DJANGO_LECTURES/Portfolio/my_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http.response import HttpResponse
+
+# 단순 함수
 def index(request):
     return HttpResponse("HELLO THIS IS A VIEW INSIDE MY APP")
