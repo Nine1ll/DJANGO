b08f9e261d5f53fc448fdac5af25ebbd0d86dc63
GwangYoung <gospotb1607@gmail.com>
2022-08-26 15:31:45 +0900

    장고 리디렉션 // 문제 해결 필요

--- DJANGO_LECTURES/Portfolio/my_app/views.py
+++ DJANGO_LECTURES/Portfolio/my_app/views.py
@@ -1,7 +1,7 @@
 from pydoc_data.topics import topics
 from django.shortcuts import render
-from django.http.response import HttpResponse, HttpResponseNotFound, Http404
-
+from django.http.response import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
 # 단순 함수
 article = {
     'sports':'Sports Page',
@@ -9,19 +9,16 @@ article = {
     'politics': 'Politics Page'
 }
 
-def news_view(request, topic):
-    try:
-        result = article[topics]
-        return HttpResponse(article[topic])
-    except :
-        # result = "No page for that topic"
-        # return HttpResponseNotFound(result)
-        raise Http404('404 GENERIC ERROR') # 404.html 과 연결 가능
-    ## You’re seeing this error because you have DEBUG = True in your Django settings file. Change that to False, and Django will display a standard 404 page.
-    ## 웹사이트 배포시 DEBUG == false
 def index(request):
     return HttpResponse("HELLO THIS IS A VIEW INSIDE MY APP")
 
+def news_view(request, topic):
+    try:
+        result = article[topic  ]
+        return HttpResponse(result)
+    except :
+        raise Http404('404 GENERIC ERROR') # 404.html 과 연결 가능
+    
 # def sport_view(request):
 #     return HttpResponse(article['sports'])
 
@@ -32,3 +29,12 @@ def add_view(request, num1, num2):
     add_result = num1 + num2
     result = f"{num1}+{num2} = {add_result}"
     return HttpResponse(str(result)) 
+
+# domain.com/my_app/0 ----> finance or sports .... 
+def num_page_view(request,num_page):
+    
+    topics_list = list(article.keys())
+    topic = topics_list[num_page]
+        
+    return HttpResponseRedirect(reverse('topic-page',args=[topic]))
+
